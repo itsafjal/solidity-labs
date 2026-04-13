048c294c1e28adbf4c2f455ae8b4a7971331f58c
= <=>
2026-04-13 01:50:19 -0400

    fund me test added

---
+++ foundry-fund-me/test/FundMeTest.t.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe FundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMiniDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e16);
+    }
+    // uint256 number = 1;
+    // function setUp() external {
+    //     number = 2;
+    // }
+    // function testDemo() public view {
+    //     console.log(number);
+    //     console.log("Hello!");
+    //     assertEq(number, 2);
+    // }
+}
